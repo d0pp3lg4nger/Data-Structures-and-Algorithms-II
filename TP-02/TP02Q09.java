3479d73b0a8515eeb0078ca8e3091817bfcd9557
default <130259647+theonedefalt@users.noreply.github.com>
2024-04-23 21:51:27 -0300

    Update TP02Q09.java

--- TP-02/TP02Q09.java
+++ TP-02/TP02Q09.java
@@ -795,7 +795,7 @@ public class TP02Q09 {
         String charset = "UTF-8";
 
         // -------------------- Ler o Arquivo .csv ---------------
-        try (BufferedReader buffer = new BufferedReader(new FileReader("/tmp/characters.csv", Charset.forName(charset)))) {
+        try (BufferedReader buffer = new BufferedReader(new FileReader("tmp/characters.csv", Charset.forName(charset)))) {
 
             String characterLine = buffer.readLine();
             while ((characterLine = buffer.readLine()) != null) {
